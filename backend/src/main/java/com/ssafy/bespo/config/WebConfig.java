368aadcc5834cbd10314a75491d14b2d69bb5828
youngseo <tim9603@naver.com>
2024-05-13 15:41:08 +0900

    Feat(S10P31A606-154): Cors 에러

---
+++ backend/src/main/java/com/ssafy/bespo/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ssafy.bespo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://bespo.co.kr")
+                .allowedOrigins("http://bespo.co.kr")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
