2cea855a970866d64013f994ab751e0814d53817
pty9714 <pty9714@naver.com>
2024-05-17 21:36:12 +0900

    메인 페이지 css 작업

--- frontend/src/pages/player/Condition.tsx
+++ frontend/src/pages/player/Condition.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PlayerSlider from '../../components/PlayerSlider';
+import { useNavigate } from 'react-router-dom';
 
 const Wrapper = styled.div`
   margin: 0% 5% 0% 5%;
@@ -32,6 +33,7 @@ const SubmitButton = styled.button`
 `;
 
 export default function Condition() {
+  const navigate = useNavigate();
   return (
     <Wrapper>
       <SliderWrapper>
@@ -39,7 +41,13 @@ export default function Condition() {
         <PlayerSlider slidername="스트레스" />
         <PlayerSlider slidername="근육상태" />
         <PlayerSlider slidername="기분" />
-        <SubmitButton>제출하기</SubmitButton>
+        <SubmitButton
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          제출하기
+        </SubmitButton>
       </SliderWrapper>
     </Wrapper>
   );
